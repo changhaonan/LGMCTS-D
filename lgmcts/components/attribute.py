93ecd947744abdc7c28d06c3bbaef82e1c0966f9
changhaonan <chnme40cs@gmail.com>
2023-08-24 00:56:06 -0400

    update: update the object selector

--- lgmcts/components/attribute.py
+++ lgmcts/components/attribute.py
@@ -1,5 +1,98 @@
 """Define a series of different attribute judger"""
 from __future__ import annotations
 from enum import Enum
+from dataclasses import dataclass
+from lgmcts.components.encyclopedia import ObjEntry, TextureEntry, SizeRange
 
-EnumType = Enum("EnumType", "color shape size")
+
+## Comparision definition
+
+class CompareRel:
+    """Compare relationship for attributes"""
+    words: list[str] = []
+
+
+class EqualRel(CompareRel):
+    """Equal relationship"""
+    words = ["equal to", "same as", "identical to", "alike", "equivalent to"]
+
+    def __eq__(self, other):
+        return isinstance(other, EqualRel)
+
+
+class DifferentRel(CompareRel):
+    """Different relationship"""
+    words = ["different with", "not the same as", "not equal to", "not identical to", "not alike", "not equivalent to", "different from"]
+
+    def __eq__(self, other):
+        """smaller, bigger is different, and verse versa"""
+        return not isinstance(other, EqualRel)
+
+
+class SmallerRel(CompareRel):
+    """Smaller relationship"""
+    words = ["smaller than", "less than", "shorter than", "narrower than", "smaller in size than"]
+
+    def __eq__(self, other):
+        return isinstance(other, SmallerRel) or isinstance(other, DifferentRel)
+
+
+class BiggerRel(CompareRel):
+    """Bigger relationship"""
+    words = ["bigger than", "larger than", "greater than", "taller than", "wider than", "bigger in size than"]
+
+    def __eq__(self, other):
+        return isinstance(other, BiggerRel) or isinstance(other, DifferentRel)
+
+
+@dataclass
+class ObjectBag:
+    """Bag of attributes for objects"""
+    obj_id: int
+    obj_name: str
+    obj_size: list[float]
+    color_name: str
+    color_value: list[float]
+
+
+def get_object_bag(obj_id: int, obj_entry: ObjEntry, texture_entry: TextureEntry) -> ObjectBag:
+    """Get object bag"""
+    obj_size = [obj_entry.size_range.high[0] - obj_entry.size_range.low[0],
+                obj_entry.size_range.high[1] - obj_entry.size_range.low[1], 
+                obj_entry.size_range.high[2] - obj_entry.size_range.low[2]]
+    return ObjectBag(obj_id, obj_entry.name, obj_size, texture_entry.name, texture_entry.color_value)
+
+
+def compare_color(obj_1: ObjectBag, obj_2: ObjectBag) -> CompareRel:
+    """Compare color"""
+    if obj_1.color_name == obj_2.color_name:
+        return EqualRel()
+    else:
+        return DifferentRel()
+
+
+def compare_size(obj_1: ObjectBag, obj_2: ObjectBag) -> CompareRel:
+    """Compare 3d size"""
+    volume_1 = obj_1.obj_size[0] * obj_1.obj_size[1] * obj_1.obj_size[2]
+    volume_2 = obj_2.obj_size[0] * obj_2.obj_size[1] * obj_2.obj_size[2]
+    if volume_1 == volume_2:
+        return EqualRel()
+    elif volume_1 < volume_2:
+        return SmallerRel()
+    else:
+        return BiggerRel()
+
+
+def compare_name(obj_1: ObjectBag, obj_2: ObjectBag) -> CompareRel:
+    """Compare name"""
+    if obj_1.obj_name == obj_2.obj_name:
+        return EqualRel()
+    else:
+        return DifferentRel()
+
+
+COMPARE_DICT = {
+    "color": compare_color,
+    "size": compare_size,
+    "name": compare_name
+}
--- lgmcts/components/prompt.py
+++ lgmcts/components/prompt.py
@@ -1,8 +1,4 @@
 """Template to generate prompt
-obj_str has two mode:
-1. Anchor: Objects with/of/has the same/similar {attribute} as {anchor}
-2. Plain: {obj_0}, {obj_1}, {obj_2}, ...
-
 We majorly care three different prompts and their combination:
 1. Region: Put/Place/Leave {obj_str} at/in/on {region}
 2. Pattern: Put/Place/Leave {obj_str} at/in/on {pattern}
@@ -17,18 +13,16 @@ import random
 
 class PromptGenerator:
     """Prompt generator"""
-    def __init__(self, rng, style="strdiff"):
+    def __init__(self, rng):
         self.rng = rng
         self.place_action_list = ["Put", "Place", "Leave"]
         self.pattern_list = ["Circle", "Square"]
         self.obj_list = ["Letter A", "Letter B", "Letter C"]
         self.prep_list = ["at", "in", "on"]
         self.correlative_list = ["; then ", "; and ", "; while ", "; so ", "; "]
-        self.attribute_list = ["color", "shape", "size"]
         self.region_prompt = ""
         self.pattern_prompt = ""
         self.pair_prompt = ""
-        self.style = style
 
     def reset(self):
         """Reset the generator"""
@@ -36,39 +30,13 @@ class PromptGenerator:
         self.pattern_prompt = ""
         self.pair_prompt = ""
 
-    ## Obj string generation
-    def gen_anchor_obj_str(self, objs: list[str]):
-        """Generate object string with anchor"""
-        anchor_obj = self.rng.choice(objs)
-        anchor_attribute = self.rng.choice(self.attribute_list)
-        prep_list = ["with", "of", "has"]
-        anchor_prep = self.rng.choice(prep_list)
-        describe_list = ["the same", "similar", "different"]
-        anchor_describe = self.rng.choice(describe_list)
-        obj_str = f"Objects {anchor_prep} {anchor_describe} {anchor_attribute} as {anchor_obj}"
-        return obj_str
-
-    def get_plain_obj_str(self, objs: list[str]):
-        """Generate object string without anchor"""
-        self.rng.shuffle(objs)
-        obj_str = ", ".join(objs)
-        return obj_str
-
     ## Prompt generation
-    def gen_pattern_prompt(self, pattern_objs: list[str], pattern: str):
+    def gen_pattern_prompt(self, obj_str: str, pattern: str):
         """Generate pattern prompt"""
-        if self.style == "strdiff":
-            obj_str = self.gen_anchor_obj_str(pattern_objs)
-        else:
-            obj_str = self.get_plain_obj_str(pattern_objs)
         self.pattern_prompt = f"{self.rng.choice(self.place_action_list)} {obj_str} {self.rng.choice(self.prep_list)} a {pattern} pattern"
 
-    def gen_region_prompt(self, region_objs: list[str], region: str):
+    def gen_region_prompt(self, obj_str: str, region: str):
         """Generate region prompt"""
-        if self.style == "strdiff":
-            obj_str = self.gen_anchor_obj_str(region_objs)
-        else:
-            obj_str = self.get_plain_obj_str(region_objs)
         self.region_prompt = f"{self.rng.choice(self.place_action_list)} {obj_str} {self.rng.choice(self.prep_list)} {region}"
 
     def gen_pair_prompt(self, obj1: str, obj2: str, relation: str):
